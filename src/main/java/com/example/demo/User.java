8c5a9f2fab72ba00442bb353ae530024093acc05
norio79 <norionorio9279@gmail.com>
2020-08-25 03:35:53 +0900

    ユーザークラスの作成

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String userId;
+	private String userName;
+	private String password;
+	private String mail;
+	private String role;
+}
