df9f39867afe65b7695520ddb07fc538067552ec
yuenee <yuenee.op@gmail.com>
2020-08-25 22:02:09 +0900

    AM-55 LoginController

---
+++ src/main/java/com/example/demo/login/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.demo.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String getLogin(Model model) {
+		
+		return "login/login";
+	}
+
+	@PostMapping("/login")
+	public String postLogin(Model model) {
+		
+		return "login/login";
+	}
+}
