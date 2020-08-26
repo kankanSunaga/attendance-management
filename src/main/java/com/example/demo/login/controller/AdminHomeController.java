c8ab67e60554964324e7f822f5133c3e1a1df991
土田裕貴 <>
2020-08-26 22:48:42 +0900

    AM-11/AM-51 AdminHomeコントローラー作成

---
+++ src/main/java/com/example/demo/login/controller/AdminHomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AdminHomeController {
+	
+	@GetMapping("/AdminHome")
+	public String getAdminHome(Model model) {
+		
+		return "admin/AdminHome";
+	}
+
+	@PostMapping("/AdminHome")
+	public String postAdminHome(Model model) {
+		
+		return "admin/AdminHome";
+	}
+}
