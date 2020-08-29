37f2f1bd851b5d7349475355e5eea9291a45d06d
Shizuka Kurosawa <gzt.myv0527@icloud.com>
2020-08-29 17:31:48 +0900

    UserDaoJdbcImplの追加

---
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.login.domain.repository.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.repository.UserDao;
+
+
+@Repository
+public class UserDaoJdbcImpl implements UserDao {
+	@Autowired
+	JdbcTemplate jdbc;
+	
+	//Userテーブルにデータを1件insert
+	@Override
+	public int insertOne(User user) throws DataAccessException {
+		
+		//1件登録
+		int rowNumber = jdbc.update("INSERT INTO m_user(user_name,"
+				+ " email,"
+				+ " password,"
+				+ " role)"
+				+ " VALUES(?, ?, ?, ?)"
+				, user.getUserName()
+				, user.getEmail()
+				, user.getPassword()
+				, user.getRole());
+		 
+		return rowNumber;
+		
+	}
+
+}
