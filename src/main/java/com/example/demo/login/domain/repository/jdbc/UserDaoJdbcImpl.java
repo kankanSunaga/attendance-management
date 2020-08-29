7c90f4791b0f832ffbf15294ddeb04c0c727caea
Shizuka Kurosawa <gzt.myv0527@icloud.com>
2020-08-30 01:06:03 +0900

    AM-14の動作確認

---
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.login.domain.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.repository.UserDao;
+
+@Repository
+public class UserDaoJdbcImpl implements UserDao {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+    // Userテーブルにデータを1件insert.
+    @Override
+    public int insertOne(User user) throws DataAccessException {
+
+        //１件登録
+        int rowNumber = jdbc.update("INSERT INTO m_user(user_name,"
+                + " email,"
+                + " password,"
+                + " role)"
+                + " VALUES(?, ?, ?, ?)",
+                user.getUserName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole());
+
+        return rowNumber;
+    }
+}
