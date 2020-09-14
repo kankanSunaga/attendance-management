2bcb973e80ea5579ffd5d18d86805a1deda3a91d
norio79 <norionorio9279@gmail.com>
2020-09-14 22:49:29 +0900

    Home画面作成 feature/AM-62/AM-176

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,27 +1,21 @@
 package com.example.demo.login.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.demo.login.domain.service.UserService;
-
 @Controller
 public class HomeController {
-	
-	@Autowired
-	UserService userService; 
-	
+
 	@GetMapping("/home")
 	public String getHome(Model model) {
 		
-		return "login/Home";
+		return "login/home";
 	}
-
+	
 	@PostMapping("/home")
-	public String postAdminHome(Model model) {
+	public String postHome(Model model) {
 		
 		return "login/home";
 	}
