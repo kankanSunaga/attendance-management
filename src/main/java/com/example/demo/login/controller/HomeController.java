7fd0b54e246886cd47d4bf89c84a174409021689
norio79 <norionorio9279@gmail.com>
2020-09-16 22:51:21 +0900

    現在の日付の表示 AM-185

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.login.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,12 @@ public class HomeController {
 
 	@GetMapping("/home")
 	public String getHome(Model model) {
+		
+		//現在の日付を取得
+		LocalDateTime today = LocalDateTime.now();
+		DateTimeFormatter formatChange = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
+		String formatChangedToday = today.format(formatChange);
+		model.addAttribute("today", formatChangedToday);
 		
 		return "login/home";
 	}
