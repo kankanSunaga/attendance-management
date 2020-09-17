15eb9802df37e3a897f9bfb7eb8a1f1b6227c1a0
土田裕貴 <>
2020-09-18 00:15:14 +0900

    コンフリクト解消

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 /*一般ユーザーテーブル*/
 INSERT INTO user (userId, userName, email, password, role, permission, frozen, requestedAt)
-VALUES(1, 'いいちゅかはやちょ', 'itsuka@xxx.co.jp', 'password', 'ROLE_GENERAL', 'false', 'false', '2020-9-01 23:25:07');
+VALUES(1, 'いいちゅかはやちょ', 'itsuka@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', 'ROLE_GENERAL', 'false', 'false', '2020-9-01 23:25:07');
 
 INSERT INTO user (userId, userName, email, password, role, permission, frozen, requestedAt)
-VALUES(3, '富田', 'tomita@xxx.co.jp', 'password', 'ROLE_GENERAL', 'false', 'false', '2020-9-01 23:25:07');
+VALUES(3, '富田', 'tomita@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', 'ROLE_GENERAL', 'false', 'false', '2020-9-01 23:25:07');
 
 /*管理者テーブル*/
 INSERT INTO user (userId, userName, email, password, role, permission, frozen, requestedAt)
-VALUES(2, '管理太郎', 'kanri@xxx.co.jp', 'password', 'ROLE_ADMIN', 'true', 'false', '2020-9-01 23:25:07');
+VALUES(2, '管理太郎', 'kanri@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', 'ROLE_ADMIN', 'true', 'false', '2020-9-01 23:25:07');
 
 /*ユーザー契約詳細テーブル*/
 INSERT INTO workDetail (workDetailId, contractTime, startTime,breakTime,endTime,startDate,officeName)
