e965335bb9d1b76a5e63a2d6efbd8a37274acbd9
土田裕貴 <>
2020-09-18 00:16:42 +0900

    コンフリクト解消

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,3 @@ VALUES(3, '富田', 'tomita@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2
 /*管理者テーブル*/
 INSERT INTO user (userId, userName, email, password, role, permission, frozen, requestedAt)
 VALUES(2, '管理太郎', 'kanri@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', 'ROLE_ADMIN', 'true', 'false', '2020-9-01 23:25:07');
-
-/*ユーザー契約詳細テーブル*/
-INSERT INTO workDetail (workDetailId, contractTime, startTime,breakTime,endTime,startDate,officeName)
-VALUES(1, 170, '09:00', '01:00', '18:00', '2020-9-01','LIM');
