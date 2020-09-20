7da41861c45b7a8d7df4e6b242ce47d0423fbc86
norio79 <norionorio9279@gmail.com>
2020-09-20 20:40:41 +0900

    合計勤務時間の表示 feature/AM-62/AM-177

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -10,12 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.WorkDetail;
 import com.example.demo.login.domain.model.WorkTime;
+import com.example.demo.login.domain.service.WorkDetailService;
 import com.example.demo.login.domain.service.WorkTimeService;
 
 @Controller
 public class HomeController {
-
+	
+	@Autowired
+	WorkDetailService workDetailService;
+	
 	@Autowired
 	private WorkTimeService workTimeService;
 	
@@ -28,16 +33,43 @@ public class HomeController {
 		String formatChangedToday = today.format(formatChange);
 		model.addAttribute("today", formatChangedToday);
 		
-		model.addAttribute("contents","login/home :: home_contents");
-		
+		//月の合計勤務時間を算出
 		List<WorkTime> monthDataList= workTimeService.selectMonthData();
+		int totalWorkTimeMinute = 0;
+		for (int i = 0; i < monthDataList.size(); i++) {
+			  WorkTime workTime = monthDataList.get(i);
+			  int workTimeMinute = workTime.getWorkTimeMinute();
+			  totalWorkTimeMinute += workTimeMinute;
+		}
 		
+		int totalWorkTimeInt = totalWorkTimeMinute / 60; //時間
+		int totalWorkTimeDouble = totalWorkTimeMinute % 60; //分
+		model.addAttribute("totalWorkTimeOfMonthInt", totalWorkTimeInt);
+		model.addAttribute("totalWorkTimeOfMonthDouble", totalWorkTimeDouble);
+		
+		//月のデータを取得する際に使用可能
 		model.addAttribute("monthDataList",monthDataList);
-		
 		System.out.println(monthDataList);
 		
-		return "login/headerLayout";
+		//契約勤務時間を取得（WorkDetailテーブルより取得）
+		WorkDetail workDetail = workDetailService.selectOne();
+		int contractTime = workDetail.getContractTime();
 		
+		//残りの契約時間の算出（契約時間から月の合計勤務時間を引く）
+		int remainingMinute = contractTime * 60 - totalWorkTimeMinute; //残り時間（分）
+		int remainingTimeInt = remainingMinute / 60; //時間
+		int remainingTimeDouble = remainingMinute % 60; //分
+		model.addAttribute("remainingTimeInt", remainingTimeInt);
+		model.addAttribute("remainingTimeDouble", remainingTimeDouble);
+		
+		//契約達成かどうかの判定
+		if(remainingTimeInt <= 0 && remainingTimeDouble <= 0) {
+			String achievement = "達成しました";
+			model.addAttribute("achievement", achievement);
+		}
+		
+		model.addAttribute("contents","login/home :: home_contents");
+		return "login/headerLayout";
 	}
 	
 	@PostMapping("/home")
