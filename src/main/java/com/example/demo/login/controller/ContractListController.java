9e0665e05f8b8dedbb44b95f701854005d946e15
norio79 <norionorio9279@gmail.com>
2020-09-25 22:46:59 +0900

    細かい修正

--- src/main/java/com/example/demo/login/controller/ContractListController.java
+++ src/main/java/com/example/demo/login/controller/ContractListController.java
@@ -13,20 +13,20 @@ public class ContractListController {
 	@Autowired
 	ContractService contractService;
 	
-	@GetMapping("/contractList")//←{userId}で動的にする
+	@GetMapping("/contractList")//{userId}で動的にする
 	public String getContractList(Model model){
 		
 		model.addAttribute("contents","login/contractList::login_contents");
 		return "login/headerLayout";
 	}
 	
-	@GetMapping("/contractMonth")//←{contractId}で動的にする
+	@GetMapping("/contractMonth")//{contractId}で動的にする
 	public String getContractMonth(Model model){
 		
 		return "login/contractMonth";
 	}
 	
-	@GetMapping("/contractDay")//←{workTimeId}で動的にする
+	@GetMapping("/contractDay")//{workTimeId}で動的にする
 	public String getContractDay(Model model){
 		
 		return "login/contractDay";
