8965f5bd5f773181c9f26e62908e5c15a8c385f7
norio79 <norionorio9279@gmail.com>
2020-09-27 15:16:43 +0900

    動的URLの作成 AM-217

--- src/main/java/com/example/demo/login/controller/ContractListController.java
+++ src/main/java/com/example/demo/login/controller/ContractListController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.login.domain.model.Contract;
 import com.example.demo.login.domain.service.ContractService;
 
 @Controller
@@ -13,21 +16,40 @@ public class ContractListController {
 	@Autowired
 	ContractService contractService;
 	
-	@GetMapping("/contractList")//{userId}で動的にする
-	public String getContractList(Model model){
+	@GetMapping("/contracts")//sessionでuserId渡されるため静的URL
+	public String getContractList(@ModelAttribute Contract form, Model model) {
+		
+		Contract contract = contractService.selectOne();
+		
+		//Contractクラスをフォームクラスに変換
+		form.setContractId(contract.getContractId());
+		form.setStartDate(contract.getStartDate());
+		form.setOfficeName(contract.getOfficeName());
+		form.setEndDate(contract.getEndDate());
+		
+		//Modelに登録
+		model.addAttribute("contractList",form);
 		
 		model.addAttribute("contents","login/contractList::login_contents");
 		return "login/headerLayout";
 	}
 	
-	@GetMapping("/contractMonth")//{contractId}で動的にする
-	public String getContractMonth(Model model){
+	@GetMapping("/contract/{contractId}")
+	public String getContractMonth(@ModelAttribute Contract form, Model model, @PathVariable("contractId")int contractId){
 		
-		return "login/contractMonth";
+		Contract contract = contractService.selectOne();
+		
+		//Contractクラスをフォームクラスに変換
+		form.setContractId(contract.getContractId());
+		
+		//Modelに登録
+		model.addAttribute("signupForm",form);
+		
+		return "login/contracts/{contractId}";
 	}
 	
-	@GetMapping("/contractDay")//{workTimeId}で動的にする
-	public String getContractDay(Model model){
+	@GetMapping("/contracts/{contractId}/{yearMonth}")
+	public String getContractDay(@ModelAttribute Contract form, Model model, @PathVariable("contractId")int contractId, @PathVariable("yearMonth")int yearMonth){
 		
 		return "login/contractDay";
 	}
