3fcb71dce62af0cde024584e9a266b62eb028e2a
yusuke-emoto <emoyuu0824@gmail.com>
2020-10-10 10:10:37 +0900

    祝日判定関数

---
+++ src/main/java/com/example/demo/login/domain/service/DayOfWeekService.java
@@ -0,0 +1,68 @@
+package com.example.demo.login.domain.service;
+
+
+import org.springframework.stereotype.Service;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+
+@Service
+public class DayOfWeekService {
+	public static boolean hasHolliday(LocalDate termination){
+		
+		//LocalDate型の変数を曜日にして、String型に変換
+		String holiday = termination.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+		//System.out.println(holiday);
+		
+		//土日だったらtrueを返す
+		if(holiday.equals("Sunday")||holiday.equals("Saturday")) {
+			return true;
+		}
+		
+		//祝日判定APIを叩いている
+		final String API_URL = "http://s-proj.com/utils/checkHoliday.php?kind=h&date=" + termination;
+
+		URL url = null;
+		BufferedReader br = null;
+		InputStreamReader isr = null;
+		String readLine = "";
+		String writeContent = "";
+
+		try {
+			url = new URL(API_URL);
+
+			/* ここから読み込むための定型文 */
+			isr = new InputStreamReader(url.openStream());
+			br = new BufferedReader(isr);
+			while ((readLine = br.readLine()) != null)
+				writeContent = writeContent + readLine;// ここで読み込んだ内容を変数に保存している
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		//System.out.println(writeContent);
+		
+		//祝日かどうかの判定
+		if (writeContent.equals("holiday")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	// 文字列の日付をフォーマット(yyyyMMddやyyyy/mm/ddなど)をもとにLocalDate型に変換するメソッド
+	public static LocalDate convertToLocalDate(String date,String format) {
+
+	                // シンプルにLocalDate型に変換された日付を返却
+	        return LocalDate.parse(date, DateTimeFormatter.ofPattern(format));
+	}
+	//テスト用
+	//LocalDate termination =convertToLocalDate("20201002", "yyyyMMdd");
+	//boolean result = hasHolliday(termination);
+	
+}
