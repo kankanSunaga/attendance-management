af0a4f0bb376a7b5db495698327bf79ffd265828
yuenee <yuenee.op@gmail.com>
2021-01-04 19:01:20 +0900

    バリデ修正

--- src/main/java/com/example/demo/login/domain/model/WorkTimeForm.java
+++ src/main/java/com/example/demo/login/domain/model/WorkTimeForm.java
@@ -3,6 +3,8 @@ package com.example.demo.login.domain.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -10,15 +12,19 @@ import lombok.Data;
 @Data
 public class WorkTimeForm {
 
+	@NotNull
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate workDay;
 
+	@NotNull
 	@DateTimeFormat(pattern = "HH:mm")
 	private LocalTime startTime;
 
+	@NotNull
 	@DateTimeFormat(pattern = "HH:mm")
 	private LocalTime breakTime;
 
+	@NotNull
 	@DateTimeFormat(pattern = "HH:mm")
 	private LocalTime endTime;
 
