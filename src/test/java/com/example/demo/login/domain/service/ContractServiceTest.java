42f657cc6f19344f1cc1f3e35ed6ef7dd6bbf6a0
yuenee <yuenee.op@gmail.com>
2021-01-13 22:36:30 +0900

    utniTest MonthService etc.

--- src/test/java/com/example/demo/login/domain/service/ContractServiceTest.java
+++ src/test/java/com/example/demo/login/domain/service/ContractServiceTest.java
@@ -11,55 +11,55 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ContractServiceTest {
-	
+
 	@Autowired
 	ContractService contractService;
 
 	@Test
-    @DisplayName("notCloseが返る")
-    void selectDisplay_1() {
+	@DisplayName("notCloseが返る")
+	void selectDisplay_1() {
 		String yearMonth = "202101";
 		boolean deadlineStatus = false;
 		boolean requestStatus = true;
-        LocalDate nowDate = LocalDate.now();
-        String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
-        String expect = "notClose";
-        assertEquals(actual, expect);
-    }
-	
+		LocalDate nowDate = LocalDate.now();
+		String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
+		String expect = "notClose";
+		assertEquals(actual, expect);
+	}
+
 	@Test
-    @DisplayName("nowRequestが返る")
-    void selectDisplay_2() {
+	@DisplayName("nowRequestが返る")
+	void selectDisplay_2() {
 		String yearMonth = "202101";
 		boolean deadlineStatus = true;
 		boolean requestStatus = true;
-        LocalDate nowDate = LocalDate.now();
-        String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
-        String expect = "nowRequest";
-        assertEquals(actual, expect);
-    }
-	
+		LocalDate nowDate = LocalDate.now();
+		String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
+		String expect = "nowRequest";
+		assertEquals(actual, expect);
+	}
+
 	@Test
-    @DisplayName("lastMonthが返る")
-    void selectDisplay_3() {
+	@DisplayName("lastMonthが返る")
+	void selectDisplay_3() {
 		String yearMonth = "202101";
 		boolean deadlineStatus = true;
 		boolean requestStatus = false;
-        LocalDate nowDate = LocalDate.of(2021, 2, 1);
-        String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
-        String expect = "lastMonth";
-        assertEquals(actual, expect);
-    }
-	
+		LocalDate nowDate = LocalDate.of(2021, 2, 1);
+		String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
+		String expect = "lastMonth";
+		assertEquals(actual, expect);
+	}
+
 	@Test
-    @DisplayName("hideが返る")
-    void selectDisplay_4() {
+	@DisplayName("hideが返る")
+	void selectDisplay_4() {
 		String yearMonth = "202101";
 		boolean deadlineStatus = true;
 		boolean requestStatus = false;
-        LocalDate nowDate = LocalDate.of(2021, 3, 1);
-        String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
-        String expect = "hide";
-        assertEquals(actual, expect);
-    }
+		LocalDate nowDate = LocalDate.of(2021, 3, 1);
+		String actual = contractService.selectDisplay(yearMonth, deadlineStatus, requestStatus, nowDate);
+		String expect = "hide";
+		assertEquals(actual, expect);
+	}
 }
--- src/test/java/com/example/demo/login/domain/service/MonthServiceTest.java
+++ src/test/java/com/example/demo/login/domain/service/MonthServiceTest.java
@@ -1,5 +1,91 @@
 package com.example.demo.login.domain.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MonthServiceTest {
 
+	@Autowired
+	MonthService monthService;
+
+	@Test
+	@DisplayName("trueが返る")
+	void deadlineCheck_1() {
+		boolean deadlineStatus = true;
+		LocalDate lastWeekDay = LocalDate.of(2021, 1, 29);
+		LocalDate nowDate = LocalDate.now();
+		boolean actual = monthService.deadlineCheck(deadlineStatus, lastWeekDay, nowDate);
+		boolean expect = true;
+		assertEquals(actual, expect);
+	}
+
+	@Test
+	@DisplayName("falseが返る")
+	void deadlineCheck_2() {
+		boolean deadlineStatus = false;
+		LocalDate lastWeekDay = LocalDate.of(2021, 1, 29);
+		LocalDate nowDate = LocalDate.of(2021, 1, 28);
+		boolean actual = monthService.deadlineCheck(deadlineStatus, lastWeekDay, nowDate);
+		boolean expect = true;
+		assertEquals(actual, expect);
+	}
+
+	@Test
+	@DisplayName("falseが返る")
+	void deadlineCheck_3() {
+		boolean deadlineStatus = false;
+		LocalDate lastWeekDay = LocalDate.of(2021, 1, 29);
+		LocalDate nowDate = LocalDate.of(2021, 1, 29);
+		boolean actual = monthService.deadlineCheck(deadlineStatus, lastWeekDay, nowDate);
+		boolean expect = false;
+		assertEquals(actual, expect);
+	}
+
+	@Test
+	@DisplayName("falseが返る")
+	void deadlineCheck_4() {
+		boolean deadlineStatus = false;
+		LocalDate lastWeekDay = LocalDate.of(2021, 1, 29);
+		LocalDate nowDate = LocalDate.of(2021, 1, 30);
+		boolean actual = monthService.deadlineCheck(deadlineStatus, lastWeekDay, nowDate);
+		boolean expect = false;
+		assertEquals(actual, expect);
+	}
+
+	@Test
+	@DisplayName("1時間30分が返る")
+	void checkQuota_1() {
+		int contractTime = 100;
+		int totalTime = 10;
+		String actual = monthService.checkQuota(contractTime, totalTime);
+		String expect = "1時間30分";
+		assertEquals(actual, expect);
+	}
+
+	@Test
+	@DisplayName("達成しましたが返る")
+	void checkQuota_2() {
+		int contractTime = 100;
+		int totalTime = 100;
+		String actual = monthService.checkQuota(contractTime, totalTime);
+		String expect = "達成しました";
+		assertEquals(actual, expect);
+	}
+
+	@Test
+	@DisplayName("達成しましたが返る")
+	void checkQuota_3() {
+		int contractTime = 100;
+		int totalTime = 110;
+		String actual = monthService.checkQuota(contractTime, totalTime);
+		String expect = "達成しました";
+		assertEquals(actual, expect);
+	}
 }
